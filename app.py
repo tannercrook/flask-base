88a0725fd9594cc367d63dcbd69ae63b1850f119
Tanner Crook <tannercrook@pop-os.localdomain>
2020-03-05 10:56:43 -0700

    Updated for dashboard template.

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask 
-from flask import render_template 
-from flask_bootstrap import Bootstrap 
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+# from flask_sqlalchemy import SQLAlchemy
 
 from models.DBase import connection 
 
 app = Flask(__name__)
-Bootstrap(app)
 
 
 # Custom class imports
-from models.objects import User, Base
-from models.Connection import db_session
+# from models.objects import User, Base
+# from models.Connection import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'url'
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'url'
+# db = SQLAlchemy(app)
 
 
 
--- models/DBase.py
+++ models/DBase.py
@@ -1,10 +1,21 @@
-import pymysql
+#import pymysql
+import psycopg2
 
+
+# MYSQL
+# def connection():
+#     conn = pymysql.connect(host="",
+#                            user = "",
+#                            passwd = "",
+#                            db = "")
+#     c = conn.cursor(pymysql.cursors.DictCursor)
+
+#     return c, conn
+
+# PostgreSQL
 def connection():
-    conn = pymysql.connect(host="",
-                           user = "",
-                           passwd = "",
-                           db = "")
-    c = conn.cursor(pymysql.cursors.DictCursor)
-
+    conn = psycopg2.connect()
+    c = conn.cursor()
     return c, conn
+
+
