7f954a61556136a49bb6a03b65be4b508a8412e8
Tanner Crook <tannercrook@gmail.com>
2020-10-22 23:18:03 -0600

    Rebuilt with self-made admin template.

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
-from flask import Flask 
-from flask import render_template 
-from flask_bootstrap import Bootstrap 
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, session
 
-from models.DBase import connection 
 
 app = Flask(__name__)
-Bootstrap(app)
+app.config['SECRET_KEY']='\xb6"\x8e\x89L\xc7\xaeJ\x80\xef\x87\xdfL\x12\xaf_m\xf0d\xfeJa\xa4%'
 
 
-# Custom class imports
-from models.objects import User, Base
-from models.Connection import db_session
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'url'
-db = SQLAlchemy(app)
 
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('dashboard/home.html')
+
+@app.route('/status')
+def status():
+    return 'Status: Running'
